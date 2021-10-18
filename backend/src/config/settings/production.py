4aa882244fb8e20f06602e763d51f31fd0fc5c02
jmartinez32 <jmartinez@shokworks.io>
2021-10-18 08:47:25 -0400

    Archivos de configuracion local y de produccion.

---
+++ backend/src/config/settings/production.py
@@ -0,0 +1,12 @@
+from base import *
+
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = False
+
+
+DEBUG = True
+
+
+DATABASES = config["DATABASES"]
--- backend/src/config/urls.py
+++ backend/src/config/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from django.contrib import admin
 import polaris.urls
+import core.testing.urls as testing_urls
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include(polaris.urls))
+    # path("", include(polaris.urls))
+    path("", include(testing_urls))
 ]
