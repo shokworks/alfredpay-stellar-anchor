593cb5f2e2e3d0b8f77d552450ab6dd4d6557994
jmartinez32 <jmartinez@shokworks.io>
2021-10-28 17:26:13 -0400

    Modificiones para SEP10 para poder realizar POST y conseguir el Token.

--- backend/src/core/testing/views.py
+++ backend/src/core/testing/views.py
@@ -21,15 +21,23 @@ from polaris.sep10.views import SEP10Auth
 from stellar_sdk import Keypair
 from stellar_sdk.client.requests_client import RequestsClient
 from stellar_sdk.sep.stellar_toml import fetch_stellar_toml
+from stellar_sdk.operation import ManageData
+
 from stellar_sdk.sep.exceptions import (
     InvalidSep10ChallengeError,
     StellarTomlNotFoundError,
+)
+from .stellar_web_authentication import (
+    build_challenge_transaction,
+    read_challenge_transaction,
+    verify_challenge_transaction_threshold,
+    verify_challenge_transaction_signed_by_client_master_key,
 )
 
 MIME_URLENCODE, MIME_JSON = "application/x-www-form-urlencoded", "application/json"
 logger = getLogger(__name__)
 
-@api_view(["GET"])
+@api_view(["GET", "HEAD"])
 @renderer_classes([PolarisPlainTextRenderer])
 def generate_toml(generate_toml):
     toml_dict1 = {
@@ -78,37 +86,31 @@ def generate_toml(generate_toml):
 
     return Response(content, content_type="text/plain")
 
+# if hasattr(self, 'get') and not hasattr(self, 'head'):
+#     self.head = self.get
+
+
 class MySEP10Auth(SEP10Auth):
 
     parser_classes = [JSONParser, MultiPartParser, FormParser]
     renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
 
     def get(self, request, *_args, **_kwargs) -> Response:
-        print(f"\n\n\n\n\n\n\n")
         account = request.GET.get("account")
-        # print(f"PASO 1 account: {account}")
         if not account:
             return Response(
                 {"error": "no 'account' provided"}, status=status.HTTP_400_BAD_REQUEST
             )
 
         home_domain = request.GET.get("home_domain")
-        # print(f"PASO 2 request.GET.get('home_domain'): {home_domain}")
         if home_domain and home_domain not in settings.SEP10_HOME_DOMAINS:
-            # print(f"PASO 3 settings.SEP10_HOME_DOMAINS: {settings.SEP10_HOME_DOMAINS}")
-            # print(f"PASO 4 Error: invalid 'home_domain' value.")
             return Response(
                 {
                     "error": f"invalid 'home_domain' value. Accepted values: {settings.SEP10_HOME_DOMAINS}"}, status=status.HTTP_400_BAD_REQUEST,)
         elif not home_domain:
             home_domain = settings.SEP10_HOME_DOMAINS[0]
-            # print(f"PASO 5 elif not home_domain: True")
-            # print(f"home_domain = settings.SEP10_HOME_DOMAINS[0]: {home_domain}")
 
         client_domain, client_signing_key = request.GET.get("client_domain"), None
-        # print(f"PASO 6 settings.SEP10_CLIENT_ATTRIBUTION_REQUIRED: {settings.SEP10_CLIENT_ATTRIBUTION_REQUIRED}")
-        # print(f"client_domain = request.GET.get('client_domain'): {client_domain}")
-        # print(f"client_signing_key: {client_signing_key}")
         if settings.SEP10_CLIENT_ATTRIBUTION_REQUIRED and not client_domain:
             return render_error_response(
                 gettext("'client_domain' is required"), status_code=400
@@ -154,7 +156,6 @@ class MySEP10Auth(SEP10Auth):
             return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
         print(f"transaction: {transaction}")
-        print(f"\n\n")
         return Response(
             {
                 "transaction": transaction,
@@ -174,19 +175,98 @@ class MySEP10Auth(SEP10Auth):
             ),
         )
         client_signing_key = client_toml_contents.get("SIGNING_KEY")
-        # print(f"PASO 8 client_toml_contents.get('SIGNING_KEY'): True")
         if not client_signing_key:
-            # print(f"PASO 9 Error")
             raise ValueError(gettext("SIGNING_KEY not present on 'client_domain' TOML"))
         try:
-            # print(f"PASO 10 Keypair.from_public_key(client_signing_key): True")
             Keypair.from_public_key(client_signing_key)
         except Ed25519PublicKeyInvalidError:
-            # print(f"PASO 11 Error")
             raise ValueError(
                 gettext("invalid SIGNING_KEY value on 'client_domain' TOML")
             )
-        print(f"PASO 12 client_signing_key: {client_signing_key}")
-        print(f"SERVER KEY GC3E")
-        print(f"CLIENT KEY GAB5")
         return client_signing_key
+
+    def post(self, request: Request, *_args, **_kwargs) -> Response:
+        envelope_xdr = request.data.get("transaction")
+        if not envelope_xdr:
+            return render_error_response(gettext("'transaction' is required"))
+        client_domain, error_response = self._validate_challenge_xdr(envelope_xdr)
+        if error_response:
+            return error_response
+        else:
+            return Response({"token": self._generate_jwt(envelope_xdr, client_domain)})
+
+    @staticmethod
+    def _validate_challenge_xdr(envelope_xdr: str):
+        logger.info("Validating challenge transaction")
+        generic_err_msg = gettext("error while validating challenge: %s")
+        try:
+            challenge = read_challenge_transaction(
+                challenge_transaction=envelope_xdr,
+                server_account_id=settings.SIGNING_KEY,
+                home_domains=settings.SEP10_HOME_DOMAINS,
+                web_auth_domain=urlparse(settings.HOST_URL).netloc,
+                network_passphrase=settings.STELLAR_NETWORK_PASSPHRASE,
+            )
+        except (InvalidSep10ChallengeError, TypeError) as e:
+            return None, render_error_response(generic_err_msg % (str(e)))
+
+        client_domain = None
+        for operation in challenge.transaction.transaction.operations:
+            if (
+                isinstance(operation, ManageData)
+                and operation.data_name == "client_domain"
+            ):
+                client_domain = operation.data_value.decode()
+                break
+
+        try:
+            account = settings.HORIZON_SERVER.load_account(challenge.client_account_id)
+        except NotFoundError:
+            logger.info("Account does not exist, using client's master key to verify")
+            try:
+                verify_challenge_transaction_signed_by_client_master_key(
+                    challenge_transaction=envelope_xdr,
+                    server_account_id=settings.SIGNING_KEY,
+                    home_domains=settings.SEP10_HOME_DOMAINS,
+                    web_auth_domain=urlparse(settings.HOST_URL).netloc,
+                    network_passphrase=settings.STELLAR_NETWORK_PASSPHRASE,
+                )
+                if (client_domain and len(challenge.transaction.signatures) != 3) or (
+                    not client_domain and len(challenge.transaction.signatures) != 2
+                ):
+                    raise InvalidSep10ChallengeError(
+                        gettext(
+                            "There is more than one client signer on a challenge "
+                            "transaction for an account that doesn't exist"
+                        )
+                    )
+            except InvalidSep10ChallengeError as e:
+                logger.info(
+                    f"Missing or invalid signature(s) for {challenge.client_account_id}: {str(e)}"
+                )
+                return None, render_error_response(generic_err_msg % (str(e)))
+            else:
+                logger.info("Challenge verified using client's master key")
+                return client_domain, None
+
+        signers = account.load_ed25519_public_key_signers()
+        threshold = account.thresholds.med_threshold
+        print(f"threshold = account.thresholds.med_threshold: {threshold}")
+        try:
+            signers_found = verify_challenge_transaction_threshold(
+                challenge_transaction=envelope_xdr,
+                server_account_id=settings.SIGNING_KEY,
+                home_domains=settings.SEP10_HOME_DOMAINS,
+                web_auth_domain=urlparse(settings.HOST_URL).netloc,
+                network_passphrase=settings.STELLAR_NETWORK_PASSPHRASE,
+                threshold=threshold,
+                signers=signers,
+            )
+        except InvalidSep10ChallengeError as e:
+            return None, render_error_response(generic_err_msg % (str(e)))
+
+        logger.info(
+            f"Challenge verified using account signers: {[s.account_id for s in signers_found]}"
+        )
+        return client_domain, None
+
