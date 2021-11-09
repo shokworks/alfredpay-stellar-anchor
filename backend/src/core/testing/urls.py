31cf72f78cc768f1e6e1429f4e7517dc6f320078
jmartinez32 <jmartinez@shokworks.io>
2021-11-09 18:17:22 -0400

    Incluido el SEP24 dentro de las pruebas y se agrega un script para depositos o retiros de prueba.

--- backend/src/core/testing/urls.py
+++ backend/src/core/testing/urls.py
@@ -3,5 +3,6 @@ from .views import generate_toml, MySEP10Auth
 
 urlpatterns = [
     path(".well-known/stellar.toml", generate_toml),
-    path("auth", MySEP10Auth.as_view())
+    path("auth", MySEP10Auth.as_view()),
+    path("sep24/", include("polaris.sep24.urls")),
 ]
