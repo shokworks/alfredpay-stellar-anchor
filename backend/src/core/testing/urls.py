8bf1d1f062a79dd6c8110c51bb096058fc55cf3c
jmartinez32 <jmartinez@shokworks.io>
2021-11-24 01:10:38 +0000

    Cambios para pasar prueba del SEP-10 y 24 con la pagina de pruebas de la comunidad de Stellar.

--- backend/src/core/testing/urls.py
+++ backend/src/core/testing/urls.py
@@ -3,6 +3,7 @@ from .views import generate_toml, MySEP10Auth
 
 urlpatterns = [
     path(".well-known/stellar.toml", generate_toml),
-    path("auth", MySEP10Auth.as_view()),
+    path("auth", include("polaris.sep10.urls")),
+    path("auth-old", MySEP10Auth.as_view()),
     path("sep24/", include("polaris.sep24.urls")),
 ]
