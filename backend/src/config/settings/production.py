64e8ca6a3ec91cbdc25e97073a481bc775752995
jmartinez32 <jmartinez@shokworks.io>
2021-11-30 00:30:21 +0000

    Se agrego funcion logging a produccion!

--- backend/src/config/settings/production.py
+++ backend/src/config/settings/production.py
@@ -10,3 +10,52 @@ DEBUG = False
 
 
 DATABASES = config["DATABASES"]
+
+
+LOGGING = {
+    "version": 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'simple': {
+            'format': '{levelname} {message}',
+            'style': '{',
+        },
+    },
+    "loggers": {
+        "testing": {
+            "level": "DEBUG",
+            "handlers": [
+                "console", "testing_info", "testing_debug",
+            ]
+        },
+        "polaris": {
+            "level": "DEBUG",
+            "handlers": [
+                "console", "polaris",
+            ]
+        }
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+        "polaris": {
+            "level": "DEBUG",
+            "class": "logging.FileHandler",
+            "filename": "./etc/log/polaris.log",
+            "formatter": "simple"
+        },
+        "testing_info": {
+            "level": "INFO",
+            "class": "logging.FileHandler",
+            "filename": "./etc/log/testing_info.log",
+            "formatter": "simple"
+        },
+        "testing_debug": {
+            "level": "DEBUG",
+            "class": "logging.FileHandler",
+            "filename": "./etc/log/testing_debug.log",
+            "formatter": "simple"
+        },
+    },
+}
