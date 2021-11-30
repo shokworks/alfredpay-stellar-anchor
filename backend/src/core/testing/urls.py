8b2ccc580c7198173479c5196f13461ba8e4ee7a
jmartinez32 <jmartinez@shokworks.io>
2021-11-30 00:30:51 +0000

    correcciones menores a las urls!

--- backend/src/core/testing/urls.py
+++ backend/src/core/testing/urls.py
@@ -1,9 +1,20 @@
+from django.conf import settings
 from django.urls import path, include
+from django.contrib import admin
+import polaris.urls
+
 from .views import generate_toml, MySEP10Auth
+
 
 urlpatterns = [
     path(".well-known/stellar.toml", generate_toml),
     path("auth", include("polaris.sep10.urls")),
-    path("auth-old", MySEP10Auth.as_view()),
+    path("auth2", MySEP10Auth.as_view()),
     path("sep24/", include("polaris.sep24.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path('admin/', admin.site.urls),
+    ]
+
