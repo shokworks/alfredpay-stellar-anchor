02da4fa6511498f587b39bbaa4c32e05f05d8b6c
josean7link <josean7link@gmail.com>
2021-12-06 13:04:34 -0400

    Borrado rastros de debug-tool.

--- backend/src/config/urls.py
+++ backend/src/config/urls.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.urls import path, include
 from django.contrib import admin
-import debug_toolbar
 import polaris.urls
 import core.testing.urls as testing_urls
 
@@ -14,5 +13,4 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += [
         path('admin/', admin.site.urls),
-        path('__debug__/', include(debug_toolbar.urls)),
     ]
